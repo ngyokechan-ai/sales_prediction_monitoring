f72dbe1729c1cec9ca5dba8a7ec3c8a2f5431359
Ashley Ng <ngyokechan@graduate.utm.my>
2026-01-17 15:14:01 +0800

    Simplified and cleaned up scripts

--- train_model_v2.py
+++ train_model_v2.py
@@ -54,6 +54,7 @@ preprocess = ColumnTransformer([
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+logging.info(f"Train set: {X_train.shape}, Test set: {X_test.shape}")
 
 # -----------------------------
 # Build pipeline: preprocessing + Random Forest Regressor
